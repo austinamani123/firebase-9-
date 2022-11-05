5349bd3cccf11282642c29ffd4225cd6842dc312
Austin Amani <amaniaustin123@gmail.com>
2022-11-05 16:48:08 +0300

    Setting up Firebase

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-import './style.css'
+// import the functions you need from the SDK's you need
+import { initializeApp } from 'firebase/app';
+import './style.css';
+
+// the web app's firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyBRCTNbGahQwFdGMehh-_TLXpiJbSjMAOU',
+	authDomain: 'fir-9-dojo-272d5.firebaseapp.com',
+	projectId: 'fir-9-dojo-272d5',
+	storageBucket: 'fir-9-dojo-272d5.appspot.com',
+	messagingSenderId: '499715572872',
+	appId: '1:499715572872:web:5774d2b4717fec53f00b3d',
+};
+
+// initializefirebase
+initializeApp(firebaseConfig);
