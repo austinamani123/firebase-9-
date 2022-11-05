6fbafa01fccfaf27c7dc2c004936f3d79f75225a
Austin Amani <amaniaustin123@gmail.com>
2022-11-05 17:28:01 +0300

    Firestore setup and data fetching

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // import the functions you need from the SDK's you need
 import { initializeApp } from 'firebase/app';
 import './style.css';
+import { getDocs, getFirestore, collection } from 'firebase/firestore';
 
 // the web app's firebase configuration
 const firebaseConfig = {
@@ -12,5 +13,24 @@ const firebaseConfig = {
 	appId: '1:499715572872:web:5774d2b4717fec53f00b3d',
 };
 
-// initializefirebase
+// initialize firebase app
 initializeApp(firebaseConfig);
+
+// init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, 'books');
+
+// get collection data
+getDocs(colRef)
+	.then((snapshot) => {
+		let books = [];
+		snapshot.docs.forEach((doc) => {
+			books.push({ ...doc.data(), id: doc.id });
+		});
+		console.log(books);
+	})
+	.catch((err) => {
+		console.log(err.message);
+	});
